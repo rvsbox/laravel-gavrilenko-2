688d18b48abdf78c8f7db4e6fa4b5babd0d96f56
rvsbox <rvsbox@live.ru>
2019-10-17 10:25:25 +0200

    05 refactoring. Error: Table not found

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Page;
+use App\Models\People;
+use App\Models\Portfolio;
+use App\Models\Service;
 
 class IndexController extends Controller
 {
-    public function execute(Request $request) {
+    //
 
-        return view('layouts.site');
+    public function execute(Request $request)
+    {
+        $pages = Page::all();
+        $portfolios = Portfolio::get(array('name', 'filter', 'images'));
+        $services = Service::where('id', '<', 20)->get();
+        $peoples = People::take(3)->get();
+
+        dd($pages);
+
+        $menu = array();
+        foreach ($pages as $page) {
+
+        }
+
+        return view('site.index');
     }
 }
