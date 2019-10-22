f9972c6f7a15d9b52d04a11ea7714b3e9e85cc50
rvsbox <rvsbox@live.ru>
2019-10-22 21:43:27 +0200

    13 test: admin/pages/edit/4, PagesEditController

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Page;
 
 class PagesEditController extends Controller
 {
-    //
+    public function execute($id)
+    {
+
+        $page = Page::find($id);
+
+        dd($page);
+
+    }
 }
