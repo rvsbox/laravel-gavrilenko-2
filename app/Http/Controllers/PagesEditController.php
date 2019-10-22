0b2edb2fceb5dfe323bc01d5874a40668a9e8bb0
rvsbox <rvsbox@live.ru>
2019-10-22 22:30:47 +0200

    14 test: admin/pages/edit/4, PagesEditController

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -12,6 +12,15 @@ class PagesEditController extends Controller
 
         //$page = Page::find($id);
 
+        if ($request->isMethod('post')) {
+
+            // вытаскиваем данные, которые хранятся в полях $request
+            // нас не интересует поле _token, те ключ
+            $input = $request->except('_token');
+
+            dd($input);
+        }
+
         // получим значение из БД конкретной записи
         $old = $page->toArray();
 
@@ -19,7 +28,7 @@ class PagesEditController extends Controller
 
             $data = [
                 'title' => 'Редактирование страницы - '.$old['name'],
-                'data'=> $old
+                'data' => $old,
             ];
 
             return view('admin.pages_edit', $data);
