2cee96e0b44ec6fb17decb3c7bf12576ebcc50a9
rvsbox <rvsbox@live.ru>
2019-10-22 22:53:26 +0200

    14 test-2: admin/pages/edit/4, PagesEditController. Validator

--- app/Http/Controllers/PagesEditController.php
+++ app/Http/Controllers/PagesEditController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 use App\Models\Page;
+
 
 class PagesEditController extends Controller
 {
@@ -18,7 +20,21 @@ class PagesEditController extends Controller
             // нас не интересует поле _token, те ключ
             $input = $request->except('_token');
 
-            dd($input);
+            // валидация
+            $validator = Validator::make($input, [
+
+                // правила валидации. См видео 14-4:10
+                'name' => 'required|max:255',
+                'alias' => 'required|max:255|unique:pages,alias,'.$input['id'],
+                'text' => 'required',
+            ]);
+
+            if($validator->fails()) {
+                return redirect()
+                    ->route('pagesEdit',['page'=>$input['id']])
+                    ->withErrors($validator);
+            }
+            
         }
 
         // получим значение из БД конкретной записи
